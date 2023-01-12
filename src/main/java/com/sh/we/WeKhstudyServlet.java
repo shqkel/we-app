3158262e4d3ffef6b58c0a27031089f8ddd000b8
shqkel <shqkel1863@gmail.com>
2023-01-12 12:40:55 +0900

    WeKhstudyServlet 추가

---
+++ src/main/java/com/sh/we/WeKhstudyServlet.java
@@ -0,0 +1,28 @@
+package com.sh.we;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WeShqkelServlet
+ */
+@WebServlet("/world")
+public class WeKhstudyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter()
+			.append("Served at: ")
+			.append(request.getContextPath())
+			.append("khstudy이 /world에 응답했습니다.");
+	}
+
+}
